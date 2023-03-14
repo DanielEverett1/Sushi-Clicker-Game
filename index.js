21b1c98f63b93976a6b3a05b0d1c4f34705acd29
DanielEverett1 <kathpromotions@gmail.com>
2023-03-13 22:06:06 -0400

    Updated

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const l = {
     cost: {
         base: 100,
         next: 100,
-        rate: .75
+        rate: 0.75
     },
     output: {
         base: 15,
@@ -71,7 +71,7 @@ const i = {
         cost: {
             base: 30,
             next: 30,
-            rate: ".50"
+            rate: 0.50
         },
         output: {
             base: 1,
